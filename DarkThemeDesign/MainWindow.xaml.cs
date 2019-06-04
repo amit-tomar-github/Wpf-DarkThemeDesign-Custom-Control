03ccbeae4ff7b59e3b87a13fd225afa80892e166
Amit Tomar <theamittomar@gmail.com>
2019-06-04 12:38:53 +0530

    Add project files.

---
+++ DarkThemeDesign/MainWindow.xaml.cs
@@ -0,0 +1,66 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Data;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+
+namespace DarkThemeDesign
+{
+    /// <summary>
+    /// Interaction logic for MainWindow.xaml
+    /// </summary>
+    public partial class MainWindow : Window
+    {
+        public MainWindow()
+        {
+            InitializeComponent();
+        }
+
+        private void button2_Click(object sender, RoutedEventArgs e)
+        {
+            Environment.Exit(0);
+        }
+
+        private void Window_Loaded(object sender, RoutedEventArgs e)
+        {
+            DataTable dt = new DataTable();
+            dt.Columns.Add("ID");
+            dt.Columns.Add("NAME");
+           
+            for (int i = 1; i < 50; i++)
+            {
+                cmbTest.Items.Add("Amit Tomar " + i.ToString());
+                listBox.Items.Add("Amit Tomar " + i.ToString());
+                dt.Rows.Add("2323", "Amit Tomar " + i.ToString());
+            }
+            dg.ItemsSource = dt.DefaultView;
+        }
+
+        private void Button_Click(object sender, RoutedEventArgs e)
+        {
+            try
+            {
+                MessageBox.Show("You clicked");
+                DataRowView rv = dg.CurrentItem as DataRowView;
+                if (rv != null)
+                {
+                    MessageBox.Show(rv["NAME"].ToString());
+                }
+            }
+            catch (Exception ex)
+            {
+                MessageBox.Show(ex.Message);
+            }
+        }
+    }
+}
